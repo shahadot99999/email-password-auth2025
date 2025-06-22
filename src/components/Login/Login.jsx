07cd50e0afdc975d57cfec233ebad4eae625dfa8
shahadot99999 <shahadot99999@gmail.com>
2025-06-22 19:45:46 +0600

    project and router setup

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h2 className="text-4xl">This is Login.</h2>
+        </div>
+    );
+};
+
+export default Login;
