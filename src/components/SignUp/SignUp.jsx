79c2e6828afca2a7db27feb61bb97f1f90953792
shahadot99999 <shahadot99999@gmail.com>
2025-06-25 08:10:50 +0600

    recap create user using email and password

---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,51 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import { auth } from '../../firebase.init';
+
+const SignUp = () => {
+
+    const [errorMessage, setErrorMessage]= useState('');
+
+    const handleSignUp = e=>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        //console.log(e.target.email.value);
+        //concole.log(e.target.password.value);
+        console.log(email, password);
+
+       //reset error and status
+       setErrorMessage('');
+
+        //create user with email and password
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(result=>{
+            console.log(result.user);
+        })
+        .catch(error=>{
+            console.log('Error', error.message);
+            setErrorMessage(error.message)
+        })
+    }
+
+    return (
+        <div className="card bg-base-100 w-full mx-auto max-w-sm shrink-0 shadow-2xl">
+            <h3 className="text-2xl ml-4 font-bold">Sign Up now!</h3>
+            <div  className="card-body">
+                <form onSubmit={handleSignUp} className="form">
+                    <label className="label">Email</label>
+                    <input type="email" name='email' className="input" placeholder="Email" />
+                    <label className="label">Password</label>
+                    <input type="password" name='password' className="input" placeholder="Password" />
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button type="submit" className="btn btn-neutral mt-4">Sign Up</button>
+                </form>
+                {
+                    errorMessage && <p className='tesxt-red-600'>{errorMessage}</p>
+                }
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import Main from './layout/Main/Main.jsx';
 import Home from './components/Home/Home.jsx';
 import Login from './components/Login/Login.jsx';
 import Register from './components/Register/Register.jsx';
+import SignUp from './components/SignUp/SignUp.jsx';
 
 
 const router = createBrowserRouter([
@@ -29,6 +30,10 @@ const router = createBrowserRouter([
       {
         path: 'register',
         element: <Register></Register>
+      },
+      {
+        path: 'signUp',
+        element: <SignUp></SignUp>
       }
     ]
   },
